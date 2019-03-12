1f7ec1a3b3646492f3ebd6280903763c323bb71c
Annika <karlsson.e.annika@gmail.com>
2019-03-12 15:10:08 +0100

    new component

--- Frontend/components/App.vue
+++ Frontend/components/App.vue
@@ -2,18 +2,20 @@
 <div>
     <counter-button />
     <search-field></search-field>
-
+    <add-book></add-book>
   </div>
 </template>
 
 <script>
     import CounterButton from './CounterButton.vue'
     import Search from './search.vue'
+    import AddBook from './addBook.vue'
 
   export default {
     components: {
       'counter-button': CounterButton,
       'search-field': Search,
+      'add-book': AddBook,
     }
   }
 </script>
