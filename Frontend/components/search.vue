3d6d0bcab6e846778e88b0b692021a9c26e2c6d3
Sara <sara.dalsman>
2019-03-12 12:34:25 +0100

    search component added

---
+++ Frontend/components/search.vue
@@ -0,0 +1,20 @@
+<template>
+  <input type="text" name="" value="Text">
+
+</template>
+<script>
+
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
+<style scoped>
+  
+</style>
