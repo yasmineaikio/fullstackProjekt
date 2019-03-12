9c7005947939eb39f4b76520fa8f5c72e6e19b9d
Annika <karlsson.e.annika@gmail.com>
2019-03-12 15:07:53 +0100

    new component

---
+++ Frontend/components/addBook.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <input type="button" value="Lägg till bok" v-on:click="onClick">
+  </div>
+</template>
+<script>
+
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+      onClick(){
+          alert()
+      }
+    }
+  }
+</script>
+<style scoped>
+</style>
