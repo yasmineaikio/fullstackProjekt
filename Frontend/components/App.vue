f9bf5eaedbfd9f79af6eb2a343308f10a71a528c
Alexander <alex.maktabi@icloud.com>
2019-03-12 21:02:04 +0100

    update

--- Frontend/components/App.vue
+++ Frontend/components/App.vue
@@ -1,15 +1,16 @@
 <template>
 <div>
-    <counter-button />
+    <counter-button/>
     <search-field></search-field>
     <add-book></add-book>
     <user-profile></user-profile>
-
+    <users/>
   </div>
 </template>
 
 <script>
     import CounterButton from './CounterButton.vue'
+    import Users from './users.vue'
     import Search from './search.vue'
     import AddBook from './addBook.vue'
     import Userprofile from './Userprofile.vue'
@@ -20,6 +21,7 @@
       'search-field': Search,
       'add-book': AddBook,
       'user-profile': Userprofile,
+      'users': Users,
     }
   }
 </script>
