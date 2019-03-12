1639f35c23076732cccbd17ef8e0fc71c2d06a89
Maija Rönkkö <ampsum@gmail.com>
2019-03-12 14:58:55 +0100

    added userprofile.vue

--- Frontend/components/App.vue
+++ Frontend/components/App.vue
@@ -2,6 +2,7 @@
 <div>
     <counter-button />
     <search-field></search-field>
+    <user-profile></user-profile>
 
   </div>
 </template>
@@ -9,11 +10,13 @@
 <script>
     import CounterButton from './CounterButton.vue'
     import Search from './search.vue'
+    import Userprofile from './Userprofile.vue'
 
   export default {
     components: {
       'counter-button': CounterButton,
       'search-field': Search,
+      'user-profile': Userprofile,
     }
   }
 </script>
