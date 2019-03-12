deb5bbf94bcb00128f1ebe4deabf4bfc33ce2348
Alexander <alex.maktabi@icloud.com>
2019-03-12 10:39:45 +0100

    update

--- Frontend/components/App.vue
+++ Frontend/components/App.vue
@@ -1,9 +1,19 @@
 <template>
- 
+<div>
+    <counter-button />
+    <counter-button />
+    <counter-button />
+  </div>
 </template>
 
 <script>
-  
+    import CounterButton from './CounterButton.vue'
+
+  export default {
+    components: {
+      'counter-button': CounterButton
+    }
+  }
 </script>
 
 <style>
---
+++ Frontend/components/counterButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <input v-bind:value="counter" v-on:click="onClick" type="button">
+</template>
+<script>
+
+  export default {
+    data() {
+      return {
+        counter: 0
+      }
+    },
+    methods: {
+      onClick() {
+        this.counter = this.counter + 1
+      }
+    }
+  }
+</script>
+<style scoped>
+  input {
+    width: 100%;
+  }
+</style>
