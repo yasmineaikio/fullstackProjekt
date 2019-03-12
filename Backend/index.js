33cbdecd99d1d480f89b829fe3bdf389b794926e
Alexander <alex.maktabi@icloud.com>
2019-03-12 17:13:19 +0100

    update

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (request, response) => {
-    response.send('Test')
+app.get('/', (req, res) => {
+    res.send('hej')
   })
   
   app.listen(3000)
