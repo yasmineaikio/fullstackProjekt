e68cd5df27f831885f950508c5005b0116273d24
Elin Gustafsson <elin.b.gustafsson@gmail.com>
2019-03-13 13:57:17 +0100

    Lagt till getBooks.vue

---
+++ Frontend/components/getBooks.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+  <h1>Böcker</h1>
+  <table>
+  <tr>
+    <th>Titel</th>
+    <th>Författare</th>
+    <th>Kategori</th>
+    <th>Utgivningår</th>
+    <th>Språk</th>
+  </tr>
+  <tr>
+    <td>HÄMTA</td>
+    <td>HÄMTA</td>
+    <td>HÄMTA</td>
+    <td>HÄMTA</td>
+    <td>HÄMTA</td>
+  </tr>
+  </table>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+      onClick(){
+
+      }
+    }
+  }
+</script>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 50%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+</style>
