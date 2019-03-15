ad7e15a85066abdcf010ffdad95b7e1a88e802da
Sara <sara.dalsman>
2019-03-15 15:13:13 +0100

    added comments

--- Frontend/components/getBooks.vue
+++ Frontend/components/getBooks.vue
@@ -23,13 +23,12 @@
   export default {
     data() {
       return {
-        ????
+        //????
       }
     },
     methods: {
-      ?????(){
-
-      }
+      //?????(){
+      //}
     }
   }
 </script>
