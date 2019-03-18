fa85e7439b2e0fc1f134bf2685f74cfbf33212b2
Sara <sara.dalsman>
2019-03-18 14:09:01 +0100

    search edited

--- Frontend/components/search.vue
+++ Frontend/components/search.vue
@@ -4,6 +4,25 @@
     <div class="search-input">
       <input type="text" placeholder="Tove Jansson" v-model="searchText" v-on:keyup.enter="searchBooks">
       <font-awesome-icon icon="search" id="search-icon" v-on:click="searchBooks"/>
+
+      <p id="advanced-search" v-on:click="showAdvanced">Avancerad sökning
+      <font-awesome-icon icon="angle-down"/></p>
+      <div v-show="advanced">
+        <div class="">
+          <h2>Kategori</h2>
+            <input type="checkbox" name="" value="">Fiktion
+            <input type="checkbox" name="" value="">Fakta
+            <input type="checkbox" name="" value="">Ungdom
+            <input type="checkbox" name="" value="">Barn
+        </div>
+        <div class="">
+          <h2>Språk</h2>
+            <input type="checkbox" name="" value="">Svenska
+            <input type="checkbox" name="" value="">Engelska
+            <input type="checkbox" name="" value="">Finska
+        </div>
+      </div>
+
     </div>
 
     <div class="search-result" v-if="result">
@@ -24,6 +43,7 @@
     data() {
       return {
         searchText: '',
+        advanced: false,
         result: false,
         books: []
       }
@@ -39,6 +59,9 @@
           this.books = allBooks
           console.log(allBooks)
         })
+      },
+      showAdvanced (){
+        this.advanced = true;
       }
     }
   }
@@ -62,6 +85,9 @@
     cursor: pointer;
     position: relative;
     top: 7px;
+  }
+  #advanced-search {
+    cursor: pointer;
   }
   .search-result {
     padding: 20px;
--- Frontend/index.js
+++ Frontend/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCog } from '@fortawesome/free-solid-svg-icons'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 library.add(faCog)
 library.add(faSearch)
+library.add(faAngleDown)
 
 import App from './components/App.vue'
 
