68cdc0cc8ce7ee8b334387ca32b8af3aa29de6be
Alexander <alex.maktabi@icloud.com>
2019-03-19 10:02:39 +0100

    updated login

--- Backend/index.js
+++ Backend/index.js
@@ -20,16 +20,19 @@ app.use((request, response, next) => {
 
 app.use(bodyParser.json());
 
+var database;
 db.open('./db.db').then(database_ => {
   database = database_
+  // database.all('SELECT * FROM users').then(users => {
+  //    allUsers = users 
+  //   console.log(allUsers);
+  // })
 })
-
-var inloggade =[]
 
 // hämtar samtliga users från databasen (Alex)
 app.get('/users', (request,response) => {
-    database.all('SELECT * FROM users').then(books => {
-        response.send(books);
+    database.all('SELECT * FROM users').then(users => {
+      response.send(users);
     })
 })
 
@@ -42,37 +45,31 @@ app.post('/users', (request,response) => {
   })
 })
 
-//login validator (Alex)
+// logga in (Alex)
 app.post('/login', (request, response) => {
+  let newID = uuidv4();
   let regUser = request.body
+   database.all('SELECT * FROM users WHERE name=? AND password=?', [regUser.name, regUser.password]).then(row => {
+     if(row[0].name === regUser.name && row[0].password === regUser.password) {
+      database.all('INSERT INTO tokens VALUES(?,?)', [regUser.name, newID]).then(user => {
+        response.status(201).send(user);
+      })
+     } else {
+      response.status(404).send('')
+      console.log('Fel användernamn eller lösenord, försök igen!');
+     }
+   })
+  })
 
-  database.all('SELECT * FROM users WHERE name =? AND password =?', [regUser.name, regUser.password]).then(books => {
-    response.status(201).send(books);
-    if(regUser) {
-      inloggade.push(regUser)
-      console.log(inloggade);
-    }
+ // Hämtar inloggade (Alex) 
+app.get('/login', (request, response) => {
+    database.all('SELECT user FROM tokens').then(inloggade => {
+     response.status(201).send(inloggade);
   })
 })
 
-app.get('/login', (request, response) => {
-   function checker() {
-     for (let i = 0; i < inloggade.length; i++) {
-      var user = inloggade[i]
-      database.all('SELECT * FROM users WHERE name =? AND password =?', [user.name, user.password]).then(inloggade => {
-      response.status(201).send(inloggade);
-    })
-  }
- }
-  checker()
-})
-
-<<<<<<< HEAD
-// kör delete istället
-app.get("/logout", function(req, res) {  
-=======
+// Logga ut (Alex) NEXT
 app.get("/logout", function(req, res) {
->>>>>>> bf3b0f6306393add447305bddc31c6f61a24a9a5
   req.logout();
 
   console.log("logged out")
