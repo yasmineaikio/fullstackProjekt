1830904cdb5eaa0087b7288ebc415cd2c4c2c3ca
Annika <karlsson.e.annika@gmail.com>
2019-03-20 10:48:00 +0100

    updated add book function

--- Frontend/components/addBook.vue
+++ Frontend/components/addBook.vue
@@ -1,22 +1,56 @@
 <template>
 <div>
-  <input type="button" value="Lägg till bok" v-on:click="onClick">
-  </div>
+<form>
+    <input v-model="title" placeholder="Titel">
+    <input v-model="author" placeholder="Författare">
+    <input v-model="category" placeholder="Kategori">
+    <input v-model="year" placeholder="Utgivningsår">
+    <input v-model="language" placeholder="Språk">
+    <input v-on:click="addBook" type="submit" value="Lägg till">
+  </form>
+  <p v-if="message">Du har lagt till {{ addedTitle }}</p>
+</div>
 </template>
 <script>
-
-  export default {
-    data() {
+export default {
+  data() {
       return {
-
+          title: null,
+          author: null,
+          category: null,
+          year: null,
+          language: null,
+          available: null,
+          returndate: null,
+          id: null,
+          message: false,
+          addedTitle: null,
       }
     },
-    methods: {
-      onClick(){
-          alert()
+  methods: {
+      addBook() {
+          fetch('http://localhost:3000/books', {
+              body: '{ "title": "' + this.title + '", "author": "' + this.author +'", "category": "' + this.category + '", "year": "' + this.year + '","language": "' + this.language + '" }',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              method: 'POST'
+          })
+          .then(response => response.json())
+          .then(result => {
+            this.addedTitle = this.title
+            this.title = null
+            this.author = null
+            this.category = null
+            this.language = null
+            this.message = true
+            this.books = result
+          })
       }
-    }
   }
+}
+
 </script>
+
 <style scoped>
 </style>
