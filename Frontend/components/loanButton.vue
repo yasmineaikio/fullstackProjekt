5253603a28ce1a27b1efb6251719e14b74c151fb
Yasmine Aikio <yyaasse@hotmail.com>
2019-03-21 11:41:42 +0100

    component loanButton

---
+++ Frontend/components/loanButton.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+  <form>
+    <button v-on:click="addLoan" type="submit">Låna</button>
+  </form>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+          available: null,
+          returnDate: null,
+          ID: null
+      }
+    },
+  methods: {
+      addLoan() {
+          fetch('http://localhost:3000/loans', {
+              body: '{ "available":"hallå", "returnDate":"hej", "ID":"jaha"}',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              method: 'POST'
+          })
+          .then(response => response.json())
+          .then(result => {
+          console.log('tillagt')
+          })
+      }
+  }
+}
+</script>
+
+<style scoped>
+</style>
