4e4523c33fe8ab019ced5d307e29769737d28b6c
Maija Rönkkö <ampsum@gmail.com>
2019-03-21 10:35:28 +0100

    edit Userprofile

--- Frontend/components/Userprofile.vue
+++ Frontend/components/Userprofile.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="profilepage_main" style="width:80%; margin:auto;">
-  <h1>Hej User!</h1>
+  <div id="profilepagemain" v-if="inloggad">
+  <h1>Hej {{ this.name }}!</h1>
 
-  <div id="profilepage_userinfo" style="">
+  <div id="profilepageuserinfo">
     Namn Namn | <a href="">mail@mail.mail</a> | Adress
   </div>
 
-  <div id="profilepage_books" style="">
+  <div id="profilepagebooks">
     <h2>Lånade böcker</h2>
 
-    <table style="border:1px solid; 000; width: 100%; margin:auto;">
+    <table id="booktable" v-for="book in books">
       <tr>
         <th>Titel</th>
         <th>Författare</th>
@@ -17,31 +17,76 @@
         <th>Återlämningsdatum</th>
       </tr>
       <tr>
-        <td>HÄMTA</td>
-        <td>HÄMTA</td>
+        <td>{{ book.title }}</td>
+        <td>{{ book.author }}</td>
         <td>HÄMTA</td>
         <td>HÄMTA</td>
       </tr>
     </table>
   </div>
 
+<p>Alla användare i databasen:</p>
+<ul v-for="user in users">
+  <li>{{ user }}</li>
+</ul>
+
+
   </div>
 </template>
 <script>
 
   export default {
+  created() {
+    this.fetchresult()
+  },
     data() {
       return {
-
+        name: 'Test',
+        users: null,
+        books: null,
+        inloggad: true,
       }
     },
     methods: {
-      onClick(){
+      fetchresult() {
+        fetch('http://localhost:3000/users')
+          .then(response => response.json())
+          .then (result => {
+            this.users = result
+          })
 
-      }
+        fetch('http://localhost:3000/books')
+          .then(response => response.json())
+          .then (result => {
+            this.books = result
+          })
+
+        }
     }
   }
+
 </script>
+
+
 <style scoped>
+#profilepagemain {
+width:80%;
+margin:auto;
+}
+
+#profilepageuserinfo {
+margin:4px;
+}
+
+#profilepagebooks {
+background-color:lightsalmon;
+padding:0 4px 4px 4px;
+}
+
+#booktable {
+border:1px solid #000;
+width: 80%;
+margin:auto;
+}
 
 </style>
