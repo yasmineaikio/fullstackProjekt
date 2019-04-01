044b09db713e9694161f605f006393fc2148c016
Annika <karlsson.e.annika@gmail.com>
2019-04-01 13:49:34 +0200

    update

--- Frontend/components/editBook.vue
+++ Frontend/components/editBook.vue
@@ -1,12 +1,33 @@
 <template>
 <div>
-    <form>
+    <table>
+  <tr>
+    <th>Bokomslag</th>
+    <th>Titel</th>
+    <th>Författare</th>
+    <th>Kategori</th>
+    <th>Utgivningår</th>
+    <th>Språk</th>
+    <th>Ändra</th>
+  </tr>
+
+  <tr v-for='book in books' v-bind:key="book">
+    <td><img v-bind:src="book.image"/></td>
+    <td>{{book.title}}</td>
+    <td>{{book.author}}</td>
+    <td>{{book.category}}</td>
+    <td>{{book.year}}</td>
+    <td>{{book.language}}</td>
+    <input class="button" type="submit" value="Ändra">
+  </tr>
+  </table>
+<!--     <form>
     <div class="field">
       <div class="control">
         <input class="button" type="submit" value="Ändra">
       </div>
     </div>
-  </form>
+  </form> -->
   </div>
 </template>
 
@@ -14,37 +35,53 @@
 export default {
   data() {
       return {
-          title: null,
-          author: null,
-          category: null,
-          year: null,
-          language: null,
-          id: null,
-          image: null,
-          editable: null,
+        title: null,
+        author: null,
+        category: null,
+        year: null,
+        language: null,
+        books: [],
+        id: null,
       }
     },
-
-  methods: {
-      editBook() {
-          fetch('http://localhost:3000/books', {
-              body: '{ "title": "' + this.title + '", "author": "' + this.author +'", "category": "' 
-              + this.category + '", "year": "' + this.year + '","language": "' + this.language + '", "id": "' 
-              + this.id + '", "image": "' + this.image + '" }',
-              headers: {
-                  'Content-Type': 'application/json'
-              },
-              method: 'PUT'
-          })
-          .then(response => response.json())
-          .then(result => {
-            this.books = result
-          })
+    created() {
+        fetch('http://localhost:3000/books')
+        .then(response => response.json())
+        .then(result => {
+        this.books = result
+    })
+  },
+  methods: {  
+      getBooks() {
+        fetch('http://localhost:3000/books')
+        .then(function(response) {
+          return response.json()
+        })
       }
-  }
+    },
+    
 }
 </script>
 
-<style scoped>
 
+<style scoped>
+.container {
+ margin: 2em;
+}
+h3  {
+  width: 80%;
+  margin: auto;
+  padding: 20px
+}
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 80%;
+  margin: auto;
+}
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
 </style>
